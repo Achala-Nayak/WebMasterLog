607b5e99daad5bc03cd9dfadd021d8b12f66d19d
Achala-Nayak <achala.nayak2@gmail.com>
2024-06-14 00:19:55 +0530

    Added PostItem.jsx -Achala Nayak

--- React-JS-Projects/Intermediate/Travel-Journal/src/components/Footer.jsx
+++ React-JS-Projects/Intermediate/Travel-Journal/src/components/Footer.jsx
@@ -8,7 +8,9 @@ const Footer = () => {
         <li><Link to='posts/categories/National'>National</Link></li>
         <li><Link to='posts/categories/International'>International</Link></li>
       </ul>
-      
+      <div className="footer_copyright">
+        <small>All Rights Reserved &copy;</small>
+      </div>
 
     </footer>
   )
---
+++ React-JS-Projects/Intermediate/Travel-Journal/src/components/PostAuthor.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const PostAuthor = () => {
+  return (
+    <Link to={`/posts/users/jdnfknf`} className='post_author'>
+        <div className='post_author'>
+            <img src="" />
+        </div>
+        <div className='post-author-details'>
+          <h5>By: Achala Nayak</h5>
+          <small>Now</small> 
+        </div>
+
+
+    </Link>
+  )
+}
+
+export default PostAuthor
